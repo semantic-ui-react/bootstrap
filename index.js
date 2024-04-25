661d6229ae05433f6a27e94d5301d21b18eb99de
honest-thief <168092687+honest-thief@users.noreply.github.com>
2024-04-25 18:17:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("You probably meant `npx @semantic-ui-react/bootstrap`");
+#!/usr/bin/env node
+const os = require("os");
+
+const { username } = os.userInfo();
+
+const pkgname = `${__filename.split("/").slice(-3, -1).join("/")}`;
+
+console.log(`
+Hello ${username}!
+
+This is a demonstration of how to typosquat popular
+scoped NPM packages ("@${pkgname}") by taking control of
+similar GitHub repos ("github.com/${pkgname}").
+
+Don't worry, this is completely harmless. But at this point
+a malicious party could run whatever code.
+
+You probably wanted to run \`npx @${pkgname} ...\`.
+`);
+process.exit(1);
