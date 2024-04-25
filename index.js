775353254e9437861ddb8cc963c6030d3cb5e97f
honest-thief <168092687+honest-thief@users.noreply.github.com>
2024-04-25 18:19:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const pkg = require("./package.json");
 const { username } = os.userInfo();
 
 console.log(`
-Hello ${username}!
-
-This is a demonstration of how to typosquat popular
-scoped NPM packages ("${pkg.name}") by taking control of
-similar GitHub repos ("github.com/${pkg.name.slice(1)}").
-
-Don't worry, this is completely harmless. But at this point
-a malicious party could run whatever code.
-
-You probably wanted to run \`npx ${pkg.name} ...\`.
+❤️ Hello ${username}!
+❤️
+❤️ This is a demonstration of how to typosquat popular
+❤️ scoped NPM packages ("${pkg.name}") by taking control of
+❤️ similar GitHub repos ("github.com/${pkg.name.slice(1)}").
+❤️
+❤️ Don't worry, this is completely harmless. But at this point
+❤️ a malicious party could run whatever code.
+❤️
+❤️ You probably wanted to run \`npx ${pkg.name} ...\`.
 `);
 process.exit(1);
